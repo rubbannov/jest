145da2e0978a7c461b8f951f639d978d3d21c8db
Igor Rubanov <fomagreen@yandex.ru>
2023-09-05 10:21:59 +0300

    add

---
+++ 7.3/jest/jest.config.js
@@ -0,0 +1,17 @@
+/** @type {import('jest').Config} */
+const config = {
+  collectCoverageFrom: [
+    "**/*.{js,jsx}",
+    "!**/node_modules/**",
+    "!**/coverage/**",
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+    },
+  },
+};
+
+module.exports = config;
